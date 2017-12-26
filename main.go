75c218e8052ad2ec0901ad0ea82bf118fa983b5d
Evgeny Koshelev <yikoshe1@mts.ru>
2017-12-26 18:47:09 +0300

    storage changed to couchbase

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"encoding/json"
+
+	//"encoding/json"
 	"fmt"
 	"log"
 	"net"
@@ -9,34 +10,30 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/garyburd/redigo/redis"
+	"github.com/couchbase/gocb"
 	"github.com/miekg/dns"
 )
 
 type inventory struct {
-	IP       string   `json:"ip"`
-	Site     string   `json:"site"`
-	HostType string   `json:"hostType"`
-	Apps     []string `json:"apps"`
-	Active   bool     `json:"active"`
+	IP     string   `json:"ip"`
+	Apps   []string `json:"apps"`
+	Active bool     `json:"active"`
 }
 
-var conn redis.Conn
+var conn, _ = gocb.Connect("127.0.0.1:8091")
+var cluster = conn.Authenticate(gocb.PasswordAuthenticator{Username: "Admin", Password: "aadmin"})
+var bucket, _ = conn.OpenBucket("testbucket", "")
 
 func main() {
 
-	var err error
-	conn, err = redis.Dial("tcp", "127.0.0.1:6379")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(2)
-	}
-	server := &dns.Server{Addr: ":53", Net: "udp"}
+	server := &dns.Server{Addr: ":51", Net: "udp"}
+
 	go func() {
 		if err := server.ListenAndServe(); err != nil {
 			log.Fatalf("Failed to set udp listener %s\n", err.Error())
 		}
 	}()
+
 	dns.HandleFunc(".", handleRequest)
 
 	sig := make(chan os.Signal)
@@ -44,39 +41,38 @@ func main() {
 	s := <-sig
 	log.Fatalf("Signal (%v) received, stopping\n", s)
 }
+
 func handleRequest(w dns.ResponseWriter, r *dns.Msg) {
+
 	m := new(dns.Msg)
 	fmt.Println("handleRequest:inbound message:")
 	fmt.Printf("%+v", r)
+	fmt.Println("len questions: ", len(r.Question))
 	for _, q := range r.Question {
 		name := q.Name
-		fmt.Println(name)
-		str, err := redis.String(conn.Do("GET", name))
+		name = name[:len(name)-1]
+
+		var host inventory
+
+		_, err := bucket.Get(name, &host)
+
 		if err != nil {
 			fmt.Println(name, err)
 			m.SetReply(r)
 			fmt.Println(m.Answer)
 			w.WriteMsg(m)
+
 			return
 		}
-		var host inventory
-		err = json.Unmarshal([]byte(str), &host)
-		if err != nil {
-			fmt.Println("can't unmarshal", str, err)
-			m.SetReply(r)
-			fmt.Println(m.Answer)
-			w.WriteMsg(m)
-			return
-		}
+
 		answer := new(dns.A)
-		answer.Hdr = dns.RR_Header{Name: name, Rrtype: dns.TypeA,
-			Class: dns.ClassINET, Ttl: 3600}
-		answer.A = net.ParseIP(host.IP) //.Preference = 10
+		answer.Hdr = dns.RR_Header{Name: name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 3600}
+		answer.A = net.ParseIP(host.IP) // Preference = 10
 		m.Answer = append(m.Answer, answer)
-
 	}
-	m.SetReply(r)
 
-	fmt.Println(m.Answer)
+	m.SetReply(r)
+	fmt.Printf("%+v\n", m)
 	w.WriteMsg(m)
 }
+
